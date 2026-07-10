131cff8bcd21ac542803f9435631bd8a2e0ee82a
nagendra-17 <nagendraganeshs17@gmail.com>
2026-07-10 16:50:01 +0530

    Fix case-sensitive import

--- src/home.jsx
+++ src/home.jsx
@@ -1,6 +1,6 @@
 import { useState,useEffect } from "react";
 import BlogList from "./blog";
-import useFetch from "./usefetch";
+import useFetch from "./useFetch";
 
 const Home = () => {
  const { data:blogs, ispanding} = useFetch('http://localhost:8000/blogs');
